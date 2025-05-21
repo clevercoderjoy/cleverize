c9f3467d7c63267544df325b73dddf0d9bb6ade1
clevercoderjoy <clevercoderjoy.com>
2025-05-21 11:12:41 +0530

    done with the manifest file

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    viteStaticCopy({
+      targets: [
+        { src: "public/manifest.json", dest: "." }, // Copy manifest
+        { src: "src/assets/icon.png", dest: "." }, // Copy icon to root of dist
+      ],
+    }),
+  ],
 });
